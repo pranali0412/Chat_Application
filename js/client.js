37f38d29a4449c663d95a2ffc6fabffc324d51a6
pranali0412 <pranalibansod04@gmail.com>
2022-09-11 19:14:41 +0530

    chat_app

--- js/client.js
+++ js/client.js
@@ -1,27 +1,40 @@
 const socket = io('http://localhost:8000');
 
+
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
+var audio = new Audio('audio.mp3');
 
 const append = (message, position) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
-    messageElement.classList.add('message')
-    messageElement.classList.add(position)
-    messageContainer.append(messageElement)
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    // if (position == 'left') {
+    //     audio.play();
+    // }
 }
 
-const name = prompt("enter Your name");
+const name = prompt("enter your name");
 socket.emit('new-user-joined', name)
 
 socket.on('user-joined', name => {
     append(`${name} joined chat`, 'right')
 })
 
-socket.on('receive', data => {
-    append(`${data.name} joined chat`, 'left')
+socket.on('receive',data =>{
+    append(`${data.name}:${data.message} `, 'left')
 })
-socket.on('leave', name => {
+socket.on('left', name => {
     append(`${name} left chat`, 'right')
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,24 +1,26 @@
-const io = require('socket.io')(8000);
-// const io = require("socket.io")(8000, {
-//     cors: {
-//     origin: "https://example.com",
-//     methods: ["GET", "POST"]
-//     }
-//     });
+// const io = require('socket.io')(8000);
+//Node Server 
+const io = require("socket.io")(8000,
+    {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    });
 
 const users = {};
 
-io.on('connection', socket => {
+io.on('connect', socket => {
     socket.on('new-user-joined', name => {
-        console.log("New user",name)
+        console.log("New user", name)
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined',name); //logo ko pta chlagea user join hua hai
+        socket.broadcast.emit('user-joined', name); //logo ko pta chlagea user join hua hai
     });
     socket.on('send', message => {
-        socket.broadcast.emit('recieve', { message: message, name: user[socket.id] })
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
     });
     socket.on('disconnect', message => {
-        socket.broadcast.emit('left',users[socket.id] )
+        socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
 })
